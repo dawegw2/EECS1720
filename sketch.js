1a96ae1767a91575a7c3e825dc7ed62ef2fc8a59
dawegw2 <dawegwii@gmail.com>
2022-01-13 20:32:33 -0800

    update

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
 var w;
+var r = 0;
+var maxDim = 100;
+var dim = 0;
 
 function setup() {
   createCanvas(640, 360);
+  
+  rectMode(CENTER);
   // Make a Walker object
   w = new Walker();
 }
@@ -9,6 +14,7 @@ function setup() {
 function draw() {
   background(51);
   // Update and display object
+  r += 5;
   w.update();
   w.display();
 }
@@ -20,13 +26,30 @@ function Walker() {
 
   this.update = function() {
     // Move Walker randomly
-    var vel = createVector(random(-5, 5), random(-5, 5));
+    var vel = createVector(random(-10, 10), random(-10, 10));
     this.pos.add(vel);
   }
 
-  this.display = function() {
+  this.display = function() {     
+    if (this.pos.x > width || this.pos.x < 0) {
+      background(200, 127, 127);
+    }  
+    if (this.pos.y > height || this.pos.y < 0) {
+      background(127, 127, 200);
+    }  
+    
     // Draw Walker as circle
-    fill(255);
-    ellipse(this.pos.x, this.pos.y, 48, 48);
+    let b = map(mouseX, 0, width, 50, 150);
+    let g = map(mouseY, 0, height, 50, 200);
+    fill(200, g, b);
+    translate(this.pos.x, this.pos.y);
+    rotate(radians(r));
+    
+    if (dim < maxDim && !mouseIsPressed) {
+      dim += 5;
+    } else {
+      dim -= 1;
+    }
+    rect(0, 0, dim, dim, 20);
   }
 }
